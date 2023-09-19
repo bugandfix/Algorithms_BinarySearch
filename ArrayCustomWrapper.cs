e8d95c962cac32b39717dda58f021b1ff03a9947
Foutami <Foutami@DESKTOP-V2EVNB1>
2023-09-19 14:32:36 +0330

    Add project files.

---
+++ ArrayCustomWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_BinarySearch;
+
+public static class ArrayCustomWrapper
+{
+
+    public static int binarySearch(int[] arrayofint, int Numbertosearch)
+    {
+        
+        int ArrayStart = 0;
+        int LengthofArray = arrayofint.Length - 1;
+
+        
+        while (ArrayStart <= LengthofArray)              
+        {
+            int midofthearray = (ArrayStart + LengthofArray) / 2;    
+            if (Numbertosearch < arrayofint[midofthearray])
+                LengthofArray = midofthearray - 1;          
+                                    
+                                    
+            else if (Numbertosearch > arrayofint[midofthearray])
+                ArrayStart = midofthearray + 1;          
+                                    
+            else return midofthearray;          
+        }
+        
+        return -1;
+    }
+
+
+    public static int[] ArraySort(int[] arrayofint)
+    {
+        int temp = 0;
+        for (int i = 0; i <= arrayofint.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arrayofint.Length; j++)
+            {
+                if (arrayofint[i] > arrayofint[j])
+                {
+                    temp = arrayofint[i];
+                    arrayofint[i] = arrayofint[j];
+                    arrayofint[j] = temp;
+                }
+            }
+        }
+
+        return arrayofint;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Algorithms_BinarySearch;
+
+int[] intArray = new int[] { 11, 51, 33, 47, 8, 24, 19, 64, 43, 70, 55, 29, 62, 39, 12, 30, 85, 36, 65, 80 };
+
+var Indexofthenumber= ArrayCustomWrapper.binarySearch(ArrayCustomWrapper.ArraySort(intArray), 80);
+
+Console.WriteLine(Indexofthenumber);
+
+//Console.WriteLine("The sorted Array is :");
+
+//foreach (var item in ArrayCustomWrapper.ArraySort(intArray))
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
+
+
+
+
+
+
+        
